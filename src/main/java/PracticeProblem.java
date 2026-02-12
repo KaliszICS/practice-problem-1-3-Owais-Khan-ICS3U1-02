c203bcd2f86fee315ac9b5899056a1a4bd74ca42
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-02-12 16:16:27 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/**
+* File Name: Lesson 1.3 - Strings
+* Author: Owais Ali Khan
+* Date Created: Feb 8, 2023
+* Date Last Modified: Feb 12, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -8,19 +15,27 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+		System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+		System.out.println("* * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		String design = "          '__'\n          (oo)\n  /========//\n / || XX ||\n*  ||----||\n   VV    VV\n   ''    ''";
+		System.out.println(design);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		String word1 = "Fortnite ";
+		String word2 = "is awful";
+		System.out.print(word1);
+		System.out.println(word2);
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		String word3 = "Don't ";
+		String word4 = "@ me";
+		System.out.println(word3 + word4);
 	}
 
 
